07a6a630312239b231b6c77760bd488aeb0cd150
notomo <notomo.motono@gmail.com>
2024-10-13 14:33:07 +0900

    Small refactor youtube player container

--- src/Player.tsx
+++ src/Player.tsx
@@ -6,7 +6,7 @@ import { RangeSlider } from "./RangeSlider";
 import { SecondsInput } from "./SecondsInput";
 import { SeekButton } from "./SeekButton";
 import { getNumber } from "./parse";
-import { PlayerStates, type YouTubePlayer } from "./youtube";
+import { YoutubePlayerContainer, useYoutubePlayer } from "./youtube";
 
 const frame = 1 / 30;
 
@@ -21,9 +21,6 @@ export function PlayerRoute() {
   const end = getNumber(searchParams.get("end"));
   const [endSeconds, setEndSeconds] = useState(end);
 
-  const [player, setPlayer] = useState<YouTubePlayer | null>(null);
-  const [duration, setDuration] = useState(0);
-
   useEffect(() => {
     setSearchParams({
       v: videoId,
@@ -32,16 +29,15 @@ export function PlayerRoute() {
     });
   }, [videoId, startSeconds, endSeconds, setSearchParams]);
 
+  const { player, duration } = useYoutubePlayer({
+    videoId,
+    startSeconds,
+    endSeconds,
+  });
+
   return (
     <div className="h-svh w-svw grid grid-rows-[85%_15%] justify-center items-center">
-      <Player
-        videoId={videoId}
-        startSeconds={startSeconds}
-        endSeconds={endSeconds}
-        player={player}
-        setPlayer={setPlayer}
-        setDuration={setDuration}
-      />
+      <YoutubePlayerContainer />
       <div>
         {duration === 0 ? (
           <div className="flex w-full justify-center items-center">
@@ -81,91 +77,6 @@ export function PlayerRoute() {
           </div>
         )}
       </div>
-    </div>
-  );
-}
-
-const playerId = "player";
-
-function Player({
-  videoId,
-  startSeconds,
-  endSeconds,
-  player,
-  setPlayer,
-  setDuration,
-}: {
-  videoId: string;
-  startSeconds: number;
-  endSeconds?: number;
-  player: YouTubePlayer | null;
-  setPlayer: (player: YouTubePlayer | null) => void;
-  setDuration: (duration: number) => void;
-}) {
-  useEffect(() => {
-    window.onYoutubeStateChange = (event) => {
-      switch (event.data) {
-        case PlayerStates.ENDED: {
-          event.target.seekTo(startSeconds, true);
-          event.target.playVideo();
-          break;
-        }
-        case PlayerStates.VIDEO_CUED: {
-          setPlayer(event.target);
-          setDuration(event.target.getDuration());
-          event.target.playVideo();
-          break;
-        }
-      }
-    };
-
-    if (player !== null) {
-      player.cueVideoById({
-        videoId,
-        startSeconds,
-        endSeconds,
-      });
-      return;
-    }
-
-    window.onYouTubeIframeAPIReady = () => {
-      new window.YT.Player(playerId, {
-        videoId,
-
-        // workaround for tablet
-        width: "1000",
-        height: "563",
-
-        events: {
-          onReady: (event) => {
-            event.target.cueVideoById({
-              videoId,
-              startSeconds,
-              endSeconds,
-            });
-          },
-          onStateChange: (event) => {
-            window.onYoutubeStateChange(event);
-          },
-        },
-      });
-    };
-
-    const head = document.getElementsByTagName("head")[0];
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://www.youtube.com/iframe_api";
-    script.async = true;
-    head?.appendChild(script);
-    return () => {
-      head?.removeChild(script);
-    };
-  }, [videoId, startSeconds, endSeconds, player, setPlayer, setDuration]);
-
-  return (
-    <div className="flex items-center w-full h-full aspect-video">
-      {/* xl is workaround for tablet */}
-      <div id={playerId} className="xl:w-full xl:h-full" />
     </div>
   );
 }
---
+++ src/youtube.tsx
@@ -0,0 +1,166 @@
+import { useEffect, useState } from "react";
+
+declare global {
+  var YT: IframeApiType;
+  var onYouTubeIframeAPIReady: () => void;
+  var onYoutubeStateChange: (event: {
+    data: PlayerState;
+    target: YouTubePlayer;
+  }) => void;
+}
+
+export interface YouTubePlayer {
+  loadVideoById({
+    videoId,
+    startSeconds,
+    endSeconds,
+  }: {
+    videoId: string;
+    startSeconds?: number;
+    endSeconds?: number;
+  }): void;
+  cueVideoById({
+    videoId,
+    startSeconds,
+    endSeconds,
+  }: {
+    videoId: string;
+    startSeconds?: number;
+    endSeconds?: number;
+  }): void;
+  seekTo(startSeconds: number, allowSeekAhead: boolean): void;
+  playVideo(): void;
+  pauseVideo(): void;
+  clearVideo(): void;
+  destroy(): void;
+  getDuration(): number;
+  getCurrentTime(): number;
+  addEventListener(
+    event: "onStateChange",
+    listener: (event: { target: YouTubePlayer }) => void,
+  ): void;
+  removeEventListener(
+    event: "onStateChange",
+    listener: (event: { target: YouTubePlayer }) => void,
+  ): void;
+}
+
+export interface IframeApiType {
+  Player: {
+    new (
+      elementId: string,
+      options: {
+        videoId: string;
+        width?: string;
+        height?: string;
+        events: {
+          onReady?: (event: { target: YouTubePlayer }) => void;
+          onStateChange?: (event: {
+            data: PlayerState;
+            target: YouTubePlayer;
+          }) => void;
+        };
+      },
+    ): YouTubePlayer;
+  };
+}
+
+const PlayerStates = {
+  UNSTARTED: -1,
+  ENDED: 0,
+  PLAYING: 1,
+  PAUSED: 2,
+  BUFFERING: 3,
+  VIDEO_CUED: 5,
+} as const;
+
+type PlayerState = (typeof PlayerStates)[keyof typeof PlayerStates];
+
+const playerId = "player";
+
+export function useYoutubePlayer({
+  videoId,
+  startSeconds,
+  endSeconds,
+}: {
+  videoId: string;
+  startSeconds: number;
+  endSeconds?: number;
+}) {
+  const [player, setPlayer] = useState<YouTubePlayer | null>(null);
+  const [duration, setDuration] = useState(0);
+
+  useEffect(() => {
+    window.onYoutubeStateChange = (event) => {
+      switch (event.data) {
+        case PlayerStates.ENDED: {
+          event.target.seekTo(startSeconds, true);
+          event.target.playVideo();
+          break;
+        }
+        case PlayerStates.VIDEO_CUED: {
+          setDuration(event.target.getDuration());
+          setPlayer(event.target);
+          event.target.playVideo();
+          break;
+        }
+      }
+    };
+
+    if (player !== null) {
+      player.cueVideoById({
+        videoId,
+        startSeconds,
+        endSeconds,
+      });
+      return;
+    }
+
+    window.onYouTubeIframeAPIReady = () => {
+      new window.YT.Player(playerId, {
+        videoId,
+
+        // workaround for tablet
+        width: "1000",
+        height: "563",
+
+        events: {
+          onReady: (event) => {
+            event.target.cueVideoById({
+              videoId,
+              startSeconds,
+              endSeconds,
+            });
+          },
+          onStateChange: (event) => {
+            window.onYoutubeStateChange(event);
+          },
+        },
+      });
+    };
+
+    const head = document.getElementsByTagName("head")[0];
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://www.youtube.com/iframe_api";
+    script.async = true;
+    head?.appendChild(script);
+    return () => {
+      head?.removeChild(script);
+    };
+  }, [videoId, startSeconds, endSeconds, player]);
+
+  return {
+    player,
+    duration,
+  };
+}
+
+export function YoutubePlayerContainer() {
+  return (
+    <div className="flex items-center w-full h-full aspect-video">
+      {/* xl is workaround for tablet */}
+      <div id={playerId} className="xl:w-full xl:h-full" />
+    </div>
+  );
+}
