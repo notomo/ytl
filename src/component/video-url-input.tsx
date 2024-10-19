f9cd56d32ba13c065e4dcd63b0166b2f597912b1
notomo <notomo.motono@gmail.com>
2024-10-19 14:43:14 +0900

    Improve video id edit

--- src/component/video-url-input.tsx
+++ src/component/video-url-input.tsx
@@ -1,6 +1,6 @@
 import * as Popover from "@radix-ui/react-popover";
-import { Check } from "lucide-react";
-import { useRef, useState } from "react";
+import { ClipboardPaste } from "lucide-react";
+import { useState } from "react";
 
 export function VideoUrlInput({
   videoId,
@@ -42,41 +42,18 @@ function EdittingVideoUrlInput({
   videoId: string;
   setVideoId: (x: string) => void;
 }) {
-  const input = useRef<HTMLInputElement>(null);
-  const button = useRef<HTMLButtonElement>(null);
   return (
-    <div className="flex items-center rounded-md border border-gray-400">
-      <input
-        ref={input}
-        className="p-2 text-center outline-none"
-        type="url"
-        defaultValue={`https://www.youtube.com/watch?v=${videoId}`}
-        onKeyDown={(e) => {
-          if (e.key !== "Enter") {
-            return;
-          }
-          e.preventDefault();
-          button.current?.click();
-        }}
-      />
-      <button
-        ref={button}
-        type="button"
-        className="rounded-md bg-green-400 p-2"
-        onClick={() => {
-          const current = input.current;
-          if (current === null) {
-            return;
-          }
-
-          const url = new URL(current.value);
-          const v = url.searchParams.get("v");
-
-          setVideoId(v ?? videoId);
-        }}
-      >
-        <Check />
-      </button>
-    </div>
+    <button
+      type="button"
+      className="rounded-md p-2 flex items-center gap-2 bg-background border border-gray-500"
+      onClick={async () => {
+        const value = await navigator.clipboard.readText();
+        const url = new URL(value);
+        setVideoId(url.searchParams.get("v") ?? videoId);
+      }}
+    >
+      <ClipboardPaste />
+      Paste URL
+    </button>
   );
 }
